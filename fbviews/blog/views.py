981481f2e65c06e05c29fdb1fae3589c8b30c452
rensequitin <rensequitin@gmail.com>
2018-04-11 15:01:12 +0800

    Analyzed context

--- fbviews/blog/views.py
+++ fbviews/blog/views.py
@@ -6,5 +6,10 @@ def list_view(request):
 	template = "blog/index.html"
 	query_set = PostModel.objects.all()
 	print(query_set)
-	context = {'query_set':query_set}
+	context = {'query_set':query_set,
+			   'dict': {'name':'Mina'},
+			   'num': 143,
+			   'array_list':[123,143],
+			   'boolean': True,
+			  }
 	return render(request, template, context)
