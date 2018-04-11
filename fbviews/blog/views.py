8d5f2a7e544a0cad45122322d9e3f26c16a4c379
rensequitin <rensequitin@gmail.com>
2018-04-11 15:27:38 +0800

    Moved code to login_require function

--- fbviews/blog/views.py
+++ fbviews/blog/views.py
@@ -5,6 +5,18 @@ from django.contrib.auth.decorators import login_required
 
 # @login_required or @login_required(login_url='/login/')
 def list_view(request):	
+	template = 'blog/index.html'
+	query_set = PostModel.objects.all()	
+	context = {'query_set':query_set,
+			   'dict': {'name':'Mina'},
+			   'num': 143,
+			   'array_list':[123,143],
+			   'boolean': True,
+			  }
+	return render(request, template, context)
+
+@login_required
+def login_require_view(request):	
 	print(request.user)	
 	if not request.user.is_authenticated:
 		raise Http404		
