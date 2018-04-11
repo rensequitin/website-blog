5598c59dfd080c73df7a9fef2a7b2661763c2608
rensequitin <rensequitin@gmail.com>
2018-04-11 11:13:44 +0800

    Analyzed request object

--- fbviews/blog/views.py
+++ fbviews/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):	
+	print(request)
+	print(dir(request)) # view available methods in request
+	return HttpResponse("<h1>Hello</h1>")
