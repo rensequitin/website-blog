f9721b23f4869a9f223459465a1c3a1b09f20323
rensequitin <rensequitin@gmail.com>
2018-04-11 11:27:56 +0800

    Analyzed HttpResponse object

--- fbviews/blog/views.py
+++ fbviews/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):	
-	print(request)
-	print(dir(request)) # view available methods in request
-	return HttpResponse("<h1>Hello</h1>")
+	response = HttpResponse()
+	print(dir(response))
+	response.write("Hello")
+	response.content = "Practice" # entire content
+	response.status_code = 404 # console 404 not found
+	return response
