976b49553022156c8a07404727824ed0b7b66ccc
rensequitin <rensequitin@gmail.com>
2018-04-18 14:40:28 +0800

    Created Model

--- cbviews/blog/admin.py
+++ cbviews/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from . import models
 
-# Register your models here.
+
+@admin.register(models.PostModel)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish']
+    fields = ['title', 'slug', 'content', 'publish','view_count','publish_date','author_email','post_age']
+    list_filter = ['title']
+    search_fields = ['title']
+    readonly_fields = ['slug','post_age','updated','timestamp']
+
+    def post_age(self, obj, *args, **kwargs):
+    	return str(obj.age)
--- cbviews/blog/models.py
+++ cbviews/blog/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+# https://raw.githubusercontent.com/codingforentrepreneurs/Django-Models-Unleashed/master/src/blog/models.py
+from datetime import timedelta, datetime, date
 
-# Create your models here.
+from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.utils.encoding import smart_text
+from django.utils import timezone 
+from django.utils.text import slugify
+from django.utils.timesince import timesince
+
+PUBLISH_CHOICES = [
+        ('draft', 'Draft'),
+        ('publish', 'Publish'),
+        ('private', 'Private'),
+    ]
+
+
+class PostModel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=240, 
+                             verbose_name='Post title', 
+                             unique=True,
+                             error_messages={'unique': 'This title is not unique, please try again.',
+                               				 'blank': 'This field is not full, please try again.'},
+                             help_text='Must be a unique title.')
+    slug = models.SlugField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+    view_count = models.IntegerField(default=0)
+    publish_date = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now())
+    author_email = models.EmailField(max_length=240, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return smart_text(self.title)
+
+    @property
+    def age(self):
+        if self.publish == 'publish':
+            now = datetime.now()
+            publish_time = datetime.combine(
+                                self.publish_date,
+                                datetime.now().min.time()
+                        )
+            try:
+                difference = now - publish_time
+            except:
+                return 'Unknown'
+            if difference <= timedelta(minutes=1):
+                return 'just now'
+            return '{time} ago'.format(time= timesince(publish_time).split(', ')[0])
+        return 'Not published'
+
+def blog_post_model_pre_save_receiver(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.title) 
+
+pre_save.connect(blog_post_model_pre_save_receiver, sender=PostModel)
