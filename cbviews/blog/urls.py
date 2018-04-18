fcd8559598adf0d2b7a348699b2eb76aa578c412
rensequitin <rensequitin@gmail.com>
2018-04-18 16:29:27 +0800

    Detail View

--- cbviews/blog/urls.py
+++ cbviews/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),    
+    path('', views.IndexView.as_view(), name="index"),
+    path('detail/<slug>/', views.PostDetail.as_view(), name="detail"),
 ]
--- cbviews/blog/views.py
+++ cbviews/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import PostModel
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 class IndexView(ListView):
 	model = PostModel
 	template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+	model = PostModel
+	template_name = 'blog/detail.html'
