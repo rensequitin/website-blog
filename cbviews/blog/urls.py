7ab91608795bcc4f4148c9cf196a860e5bb2a644
rensequitin <rensequitin@gmail.com>
2018-04-19 12:32:34 +0800

    Delete View

--- cbviews/blog/urls.py
+++ cbviews/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<slug>/detail/', views.PostDetail.as_view(), name="detail"),
     path('create/', views.PostCreate.as_view(), name="create"),
     path('<slug>/update/', views.PostUpdate.as_view(), name="update"),
+    path('<slug>/delete/', views.PostDelete.as_view(), name="delete"),
 ]
--- cbviews/blog/views.py
+++ cbviews/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import PostModel
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
 from .forms import PostCreateForm
 
@@ -30,9 +30,17 @@ class PostCreate(CreateView):
 		form.instance.added_by = self.request.user 
 		return super().form_valid(form)
 
+
 class PostUpdate(UpdateView):
 	model = PostModel
 	form_class = PostCreateForm
 	template_name = 'blog/update.html'
 	def get_success_url(self):
-		return '{}'.format(reverse('blog:index'))
+		return '{}'.format(reverse('blog:detail',self.kwargs.get('slug')))
+
+
+class PostDelete(DeleteView):
+	model = PostModel
+	template_name = 'blog/delete.html'
+	def get_success_url(self):
+		return '{}'.format(reverse('blog:index'))		
